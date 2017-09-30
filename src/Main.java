c1525780a8079172a84d840a6227f7bc27798da0
future88075620 <future870914@gmail.com>
2017-09-30 14:39:41 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*
+        ;
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner in=new Scanner(System.in);
+	    int n;
+	    System.out.printf("Input:");
+	    n=in.nextInt();
+	    System.out.printf("n=%d\n",n*9/5+32);
+    }
+}
